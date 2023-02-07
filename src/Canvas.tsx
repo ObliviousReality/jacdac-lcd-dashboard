8ce7ab4d7489ef4189187e96d1738489ba3673d7
Tom <t.litherland@lancaster.ac.uk>
2023-02-07 13:04:51 +0000

    Custom scaling and started on rendering control.

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -4,6 +4,44 @@ import { useRegister, useRegisterValue, useServices } from "react-jacdac";
 import './stylesheet.css';
 import Log from "./Logger.tsx";
 
+var initialFill = false;
+
+const width = 160;
+const height = 120;
+
+var scaleFactor = 5;
+
+var context;
+var canvas;
+
+var ItemList: RenderItem[];
+
+
+class RenderItem {
+    type: string;
+    data: number[]
+
+    constructor(t: string, ...params: number[]) {
+        this.type = t;
+        this.data = params;
+    }
+
+    draw(context, scale) {
+        let d = this.data;
+        switch (this.type) {
+            case "L":
+                context.drawLine(d[0] * scale, d[1] * scale, d[2] * scale, d[3]);
+                break;
+            case "R":
+                context.drawRect(d[0] * scale, d[1] * scale, d[2] * scale, d[3]);
+                break;
+            case "C":
+                context.fillStyle(d[0], d[1], d[2]);
+        }
+    }
+}
+
+
 
 const Canvas = (props) => {
     const canvasRef = React.useRef(null);
@@ -14,46 +52,78 @@ const Canvas = (props) => {
 
     const [pressure = 0] = useRegisterValue<[number]>(buttonReg);
 
+    const scaleup = document.getElementById("scaleup") as HTMLButtonElement;
+    const scaledown = document.getElementById("scaledown") as HTMLButtonElement;
+    const scaletext = document.getElementById("scaletext") as HTMLParagraphElement;
+
+    scaleup.onclick = () => { setScale(context, scaleFactor + 1) }
+    scaledown.onclick = () => { setScale(context, scaleFactor - 1) }
+    if (ItemList != undefined) {
+        ItemList.push(new RenderItem("C", 255, 255, 0));
+        ItemList.push(new RenderItem("K", 10, 10, 100, 100));
+    }
 
     const draw = ctx => {
+        Log("Drawing");
         ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
         ctx.fillStyle = '#000000'
         ctx.fillRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+        if (ItemList != undefined) {
+            ItemList.forEach(() => draw(ctx));
+        }
     }
 
     const drawLine = ctx => {
         for (let i = 0; i < 100; i++) {
-            ctx.fillRect(i * 5, i * 5, 5, 5);
+            ctx.fillRect(i * scaleFactor, i * scaleFactor, scaleFactor, scaleFactor);
         }
     }
 
+    const setScale = (ctx, scale) => {
+        if (scale <= 0 || scale > 10) {
+            return;
+        }
+        scaleFactor = scale;
+        ctx.canvas.width = width * scaleFactor;
+        ctx.canvas.height = height * scaleFactor;
+        scaletext.innerText = "Dimensions: " + width + "x" + height + ", with scaling factor " + scaleFactor;
+        draw(ctx);
+    }
+
     React.useEffect(() => {
-        const canvas = canvasRef.current;
-        const context = canvas.getContext('2d');
-        context.canvas.width = 160 * 5;
-        context.canvas.height = 120 * 5;
-        context.fillStyle = '#000000'
-        context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+        canvas = canvasRef.current;
+        context = canvas.getContext('2d');
+        if (!initialFill) {
+            context.canvas.width = width * scaleFactor;
+            context.canvas.height = height * scaleFactor;
+            // context.fillStyle = '#000000'
+            // context.fillRect(0, 0, context.canvas.width, context.canvas.height);
+            // Log("Restting Canvas");
+            // // initialFill = true;
+        }
+
         draw(context);
         if (pressure > 0) {
             context.fillStyle = '#FFFFFF'
             // context.canvas.width = context.canvas.width * 2;
             Log("Button Pressed.");
+            // setScale(context, 1);
         }
         else {
             context.fillStyle = '#000000'
             // context.canvas.width = context.canvas.width / 2;
+            // setScale(context, 5);
         }
-        // context.fillRect(10 * 5, 10 * 5, 1 * 5, 1 * 5);
+        // context.fillRect(10 * scaleFactor, 10 * scaleFactor, 1 * scaleFactor, 1 * scaleFactor);
         drawLine(context);
 
-    }, [draw]);
+    }, [pressure, draw]);
 
 
 
     return (
         <div>
-            <canvas ref={canvasRef} {...props} />
+            <canvas id="thecanvas" ref={canvasRef} {...props} />
         </div>
     );
 }
--- src/JDConn.tsx
+++ src/JDConn.tsx
@@ -1,7 +1,36 @@
-import * as React from "react"
+import { PACKET_SEND, RotaryEncoderReg, SRV_BUTTON, SRV_ROTARY_ENCODER } from "jacdac-ts";
+import * as React from "react";
+import { useRegister, useRegisterValue, useServices } from "react-jacdac";
+import Log from "./Logger.tsx";
 
+var canvas;
+var context;
 
 const JDConn = () => {
+    canvas = document.getElementById("thecanvas") as HTMLCanvasElement;
+
+    const rotService = useServices({ serviceClass: SRV_ROTARY_ENCODER })[0];
+    const btnService = useServices({ serviceClass: SRV_BUTTON })[0];
+
+    const spinReg = useRegister(rotService, RotaryEncoderReg.Position);
+    const spin = useRegisterValue<[number]>(spinReg)
+    var prevSpin = spin;
+
+    if (canvas != null) {
+        context = canvas.getContext('2d');
+        // Log("Connected to canvas");
+        for (let i = 100; i > 1; i--) {
+            context.fillStyle = '#FF0000'
+            context.fillRect((100 * 5) - i * 5, i * 5, 5, 5);
+        }
+    }
+    // if (btnService != null) {
+    //     btnService.on(PACKET_SEND, pkt => Log(`${this} send ${pkt}`))
+    //     btnService.on(PACKET_SEND, pkt => Log(`${this} received ${pkt}`))
+    // }
+    return (
+        <p>{spin}</p>
+    )
 
 }
 
